8112ff659360fff69948a915f4cadc558e5cf92b
shashanoid <shashwat.singh@shopify.com>
2021-09-25 21:40:50 -0400

    Update S3

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -3,13 +3,13 @@ require 'carrierwave'
 
 CarrierWave.configure do |config|
   config.storage = :fog
-  # config.s3_access_key_id ="AKIAQBQ5SZ4ZHKN7TFQD"
-  # config.s3_secret_access_key = "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"
+  # config.s3_access_key_id ="ASWS2DKIAQBQDK5SZ4ZTFQD"
+  # config.s3_secret_access_key = "Rwln5VHajhsLBjbdNEaTR6LMYwMMRdsfWOzI"
   # config.s3_bucket = "poembucketphotos"
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
-    :aws_access_key_id      => 'AKIAQBQ5SZ4ZHKN7TFQD',            # required
-    :aws_secret_access_key  => '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',     # required
+    :aws_access_key_id      => 'ASWS2DKIAQBQDK5SZ4ZTFQD',            # required
+    :aws_secret_access_key  => 'Rwln5VHajhsLBjbdNEaTR6LMYwMMRdsfWOzI',     # required
     :region                 => 'us-east-2'                        # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'poembucketphotos'               # required
